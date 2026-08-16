c9d0c82fa4112dbdaaeb2d2782162ac0cabd421e
andrew <akariaka@yandex.ru>
2026-08-17 03:45:00 +0600

    create component

---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,10 @@
+<header>
+  <nav class="navbar bg-body-tertiary">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="/">Bootstrap</a>
+        <a href="/signin">вход</a>
+        <a href="/signup">регистрация</a>
+    </div>
+  </nav>
+</header>
+
