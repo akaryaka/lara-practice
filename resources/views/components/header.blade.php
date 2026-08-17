0e7d7c85f8f4612bbc14bd2836845b8229c6ef4e
andrew <akariaka@yandex.ru>
2026-08-17 22:56:08 +0600

    register bug fixed

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -2,7 +2,7 @@
   <nav class="navbar bg-body-tertiary">
     <div class="container-fluid">
         <a class="navbar-brand" href="/">Bootstrap</a>
-        <a href="/signin">вход</a>
+        <a href="/login">вход</a>
         <a href="/signup">регистрация</a>
     </div>
   </nav>
--- routes/web.php
+++ routes/web.php
@@ -11,9 +11,11 @@ Route::post('/submit', function () {
     return view('submit');
 });
 
-Route::get('/signin', function () {
-    return view('signin');
-});
+Route::get('/login', function () {
+    return view('login');
+})->name('login');
+
+Route::view('/dashboard', 'dashboard')->middleware('auth')->name('dashboard');
 
 Route::get('/signup', [SignUpController::class, 'create'])->name('signup');
 Route::post('/signup', [SignUpController::class, 'store']);
