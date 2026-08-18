26132b67683d2b369ef402d51906d78b819d3489
andrew <akariaka@yandex.ru>
2026-08-18 18:14:12 +0600

    debug signup

--- app/Http/Controllers/Auth/SignUpController.php
+++ app/Http/Controllers/Auth/SignUpController.php
@@ -19,14 +19,12 @@ class SignUpController extends Controller
         $request->validate([
             'email' => ['required', 'string', 'email', 'unique:users'],
             'password' => ['required', 'string', 'min:8'],
-            'check' => ['required']
         ]);
         // получаем поля формы регистрации, для проверки
         // dd($request -> all());
         $user = User::create([
             'email' => $request->email,
             'password' => $request->password,
-            'check' => $request->check
         ]);
 
         Auth::login($user);
