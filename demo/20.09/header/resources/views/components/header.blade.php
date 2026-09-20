bbd21919c02b9f898509803c61c62f68adcc49ca
andrew <akariaka@yandex.ru>
2026-09-21 02:47:56 +0600

    upd 02:47

---
+++ demo/20.09/header/resources/views/components/header.blade.php
@@ -0,0 +1,19 @@
+<header>
+  <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+    <div class="container">
+      <a class="navbar-brand" href="{{ url('/') }}">
+        logo
+      </a>
+      <div class="collapse navbar-collapse">
+        <ul class="navbar-nav ms-auto">
+          <li class="nav-item">
+              <a class="nav-link" href="{{ url('/') }}">Login</a>
+          </li>
+          <li class="nav-item">
+              <a class="nav-link" href="{{ url('/') }}">Register</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+</header>
