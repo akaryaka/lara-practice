b41b3c523a748ce14308542f23c649513ecd289e
andrew <akariaka@yandex.ru>
2026-09-22 23:57:46 +0600

    create MainController

---
+++ app/Http/Controllers/MainController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class MainController extends Controller
+{
+    public function Index() {
+
+    }
+
+    public function About() {
+        
+    }
+
+    public function Services() {
+        
+    }
+
+    public function Search() {
+        
+    }
+
+    public function Subscribe() {
+        
+    }
+
+    public function Callback() {
+        
+    }
+}
