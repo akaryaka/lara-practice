8beef4e06f00374dff63b4c7bb881bf37444c4dc
andrew <akariaka@yandex.ru>
2026-09-24 21:44:06 +0600

    add 3__controllers, ref another

---
+++ demo/3_controllers/resources/views/home.blade.php
@@ -0,0 +1,7 @@
+@extends("layout")
+@section('title', 'Главная')
+@section('content')
+  <div class="container">
+    <h1>Главная</h1>
+  </div>
+@endsection
---
+++ demo/3_controllers/resources/views/register.blade.php
@@ -0,0 +1,24 @@
+@extends("layout")
+@section('title', 'Регистрация')
+@section('content')
+  <div class="container">
+    <div class="d-flex flex-column align-items-center justify-content-center vh-100">
+      <h1 class="text-center">Регистрация</h1>
+      <form class="w-25">
+        <div class="form-group mb-3">
+          <label for="input-login">Логин</label>
+          <input type="text" class="form-control" id="input-login" placeholder="Введите логин">
+        </div>
+        <div class="form-group mb-3">
+          <label for="input-email">Почта</label>
+          <input type="email" class="form-control" id="input-email" placeholder="Введите email">
+        </div>
+        <div class="form-group mb-3">
+          <label for="password">Пароль</label>
+          <input type="password" class="form-control" id="password" placeholder="Введите пароль">
+        </div>
+        <button type="submit" class="btn btn-primary w-100">Зарегистрироваться</button>
+      </form>
+    </div>
+  </div>
+@endsection
